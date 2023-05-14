162ae43c7ba12f6f0c8134c8a8f06a82a1836f73
udrym <mathieu.udry@students.hevs.ch>
2023-05-14 16:34:08 +0200

    obstacles 1

--- src/main/java/Obstacles.java
+++ src/main/java/Obstacles.java
@@ -1,2 +1,19 @@
+import java.util.random
 public class Obstacles {
+        private int longueur;
+        private int hauteur;
+
+        public Obstacle(int maxlongueur, int maxhauteur) {
+            Random rand = new Random();
+            longueur= rand.nextInt(maxlongueur);
+            hauteur = rand.nextInt(maxhauteur);
+        }
+
+        public int getlongueuer() {
+            return longueur;
+        }
+
+        public int gethauteur() {
+            return hauteur;
+        }
 }
