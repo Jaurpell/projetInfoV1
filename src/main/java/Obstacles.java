b481cf0080d44fcef73d9625229d4cec6c9bd603
udrym <mathieu.udry@students.hevs.ch>
2023-05-15 16:05:36 +0200

    obstacle fin de journée 15 mai

--- src/main/java/Obstacles.java
+++ src/main/java/Obstacles.java
@@ -2,18 +2,20 @@ import java.util.random
 public class Obstacles {
         private int longueur;
         private int hauteur;
+        private int deplacement;
 
-        public Obstacle(int maxlongueur, int maxhauteur) {
+        public Obstacle(int maxlongueur) {
             Random rand = new Random();
-            longueur= rand.nextInt(maxlongueur);
-            hauteur = rand.nextInt(maxhauteur);
+            this.longueur= rand.nextInt(20-5);
+            this.hauteur= 10
+
+            this.deplacement = -5
+
         }
 
-        public int getlongueuer() {
-            return longueur;
+        public int getLongueuer() {
+            return this.longueur;
         }
-
-        public int gethauteur() {
-            return hauteur;
-        }
+        public int getHauteur() {return this.hauteur}
+        public int getDeplacementObstacle() {return this.deplacement}
 }
